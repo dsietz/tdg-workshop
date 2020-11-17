cb9b6fee579e81350a69970d5cf8d8da2eb71ab3
dsietz <davidsietz@yahoo.com>
2020-11-17 11:40:55 -0500

    working on analyzer

--- rust-tdg/src/bin/tdg-analyzer.rs
+++ rust-tdg/src/bin/tdg-analyzer.rs
@@ -3,8 +3,37 @@ extern crate clap;
 
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 use clap::{Arg, App};
+use std::path::Path;
+use test_data_generation::{Profile, shared};
 
+static WORKSPACE_LOCAL_STORAGE: &str = "../profiles";
 
+fn make_consumer(data_topic: String) -> Result<Consumer, kafka::Error> {
+    Consumer::from_hosts(vec!("localhost:9092".to_owned()))
+      .with_topic(data_topic.to_owned())
+      .with_fallback_offset(FetchOffset::Earliest)
+      .with_group("my-group".to_owned())
+      .with_offset_storage(GroupOffsetStorage::Kafka)
+      .create()
+}
+
+fn analyse_data(topic: &str, data: &str) -> Result<bool, std::io::Error>{
+  let profile_file = shared::string_to_static_str(format!("{}/{}", WORKSPACE_LOCAL_STORAGE, topic));
+  let mut profile = match Path::new(&format!("{}/{}.json", WORKSPACE_LOCAL_STORAGE, topic)).exists() {
+    true => {
+      // use existing file
+      Profile::from_file(&profile_file)
+    },
+    false => {
+      // create new file
+      println!("Creating new profile: {}",topic);
+      Profile::new_with_id(topic.to_string())
+    },
+  };
+  profile.analyze(&data);
+  profile.pre_generate();
+  profile.save(&profile_file)
+}
 
 fn main() {
     let matches = App::new("Sample Data Analyzer")
@@ -20,12 +49,7 @@ fn main() {
     let data_topic = matches.value_of("topic").unwrap();
     println!("Listening to the {} topic ...", data_topic);
     
-    let mut consumer = match Consumer::from_hosts(vec!("localhost:9092".to_owned()))
-      .with_topic(data_topic.to_owned())
-      .with_fallback_offset(FetchOffset::Earliest)
-      .with_group("my-group".to_owned())
-      .with_offset_storage(GroupOffsetStorage::Kafka)
-      .create() {
+    let mut consumer = match make_consumer(data_topic.to_string()) {
           Ok(c) => c,
           Err(err) => {
             panic!("{}",err);  
@@ -35,7 +59,17 @@ fn main() {
     loop {
       for ms in consumer.poll().unwrap().iter() {
         for m in ms.messages() {
-          println!("{}", String::from_utf8(m.value.to_vec()).unwrap());
+          let data = String::from_utf8(m.value.to_vec()).unwrap();
+          println!("{}", data);
+          match analyse_data(data_topic, &data) {
+            Ok(_) =>{
+              //do nothing
+              println!("Analyzed the data.");
+            },
+            Err(err) => {
+              println!("Unable to analyze the data for the topic {}!", data_topic);
+            },
+          }
         }
         consumer.consume_messageset(ms);
       }
